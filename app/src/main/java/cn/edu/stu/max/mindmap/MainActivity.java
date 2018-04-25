779fbbef99a09f6053af6240a0a6c5ff5370112a
LiHua <Liva_E@outlook.com>
2018-04-25 23:00:33 +0800

    添加主界面ActionBar设置选项

--- app/src/main/java/cn/edu/stu/max/mindmap/MainActivity.java
+++ app/src/main/java/cn/edu/stu/max/mindmap/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
