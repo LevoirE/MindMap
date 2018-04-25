56844c404ec23d4e81d717c6f65afbc287967c0f
LiHua <Liva_E@outlook.com>
2018-04-25 22:29:10 +0800

    创建了欢迎界面和主界面

---
+++ app/src/main/java/cn/edu/stu/max/mindmap/WelcomeActivity.java
@@ -0,0 +1,26 @@
+package cn.edu.stu.max.mindmap;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+//        getSupportActionBar().hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 2s = 2000ms
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
