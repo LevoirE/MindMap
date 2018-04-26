d330e444f4ddba3b0a4e3689a7a6492b336c3b2c
LiHua <Liva_E@outlook.com>
2018-04-26 23:29:34 +0800

    增加工作菜单缩放功能，拖动功能未完成

--- app/src/main/java/cn/edu/stu/max/mindmap/WorkActivity.java
+++ app/src/main/java/cn/edu/stu/max/mindmap/WorkActivity.java
@@ -1,21 +1,105 @@
 package cn.edu.stu.max.mindmap;
 
-import android.os.Build;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class WorkActivity extends AppCompatActivity {
-
+    private ScaleGestureDetector scaleGestureDetector = null;
+    private int mActivePointerId;
+    private int INVALID_POINTER_ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
+        mActivePointerId = INVALID_POINTER_ID =
+                ((ViewGroup)findViewById(android.R.id.content)).getChildAt(0).getId();
 
         //加入返回箭头
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureListener());
+    }
+
+
+    private float mLastTouchX;
+    private float mLastTouchY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
+        final int action = MotionEventCompat.getActionMasked(event);
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                final int pointerIndex = MotionEventCompat.getActionIndex(event);
+                final float x = MotionEventCompat.getX(event, pointerIndex);
+                final float y = MotionEventCompat.getY(event, pointerIndex);
+
+                // Remember where we started (for dragging)
+                mLastTouchX = x;
+                mLastTouchY = y;
+                // Save the ID of this pointer (for dragging)
+                mActivePointerId = MotionEventCompat.getPointerId(event, 0);
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                // Find the index of the active pointer and fetch its position
+                final int pointerIndex =
+                        MotionEventCompat.findPointerIndex(event, mActivePointerId);
+
+                final float x = MotionEventCompat.getX(event, pointerIndex);
+                final float y = MotionEventCompat.getY(event, pointerIndex);
+
+                // Calculate the distance moved
+                final float dx = x - mLastTouchX;
+                final float dy = y - mLastTouchY;
+
+
+                // Remember this touch position for the next move event
+                mLastTouchX = x;
+                mLastTouchY = y;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+
+                final int pointerIndex = MotionEventCompat.getActionIndex(event);
+                final int pointerId = MotionEventCompat.getPointerId(event, pointerIndex);
+
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mLastTouchX = MotionEventCompat.getX(event, newPointerIndex);
+                    mLastTouchY = MotionEventCompat.getY(event, newPointerIndex);
+                    mActivePointerId = MotionEventCompat.getPointerId(event, newPointerIndex);
+                }
+                break;
+            }
+        }
+        return true;
     }
 
     //实现返回功能
@@ -27,6 +111,36 @@ public class WorkActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private final class ScaleGestureListener implements
+            ScaleGestureDetector.OnScaleGestureListener {
+        private float scaleFactor = 1.0f;
+        private float preScaleFactor = 1.0f;
+
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            float previousSpan = scaleGestureDetector.getPreviousSpan();
+            float currentSpan = scaleGestureDetector.getCurrentSpan();
+            if (currentSpan < previousSpan) {
+                scaleFactor = preScaleFactor - (previousSpan - currentSpan) / 2000;
+            } else {
+                scaleFactor = preScaleFactor + (currentSpan - previousSpan) / 2000;
+            }
+            ((ViewGroup)findViewById(android.R.id.content)).getChildAt(0).setScaleX(scaleFactor);
+            ((ViewGroup)findViewById(android.R.id.content)).getChildAt(0).setScaleY(scaleFactor);
+            return false;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+            preScaleFactor = scaleFactor;
+        }
     }
 
     //活动转换之间都调用沉浸模式全屏
