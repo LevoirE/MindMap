7917c118c38f126ea31c6341d0b17b2221df0a3a
LiHua <Liva_E@outlook.com>
2018-04-26 09:31:08 +0800

    修改欢迎界面沉浸全屏模式

--- app/src/main/java/cn/edu/stu/max/mindmap/WelcomeActivity.java
+++ app/src/main/java/cn/edu/stu/max/mindmap/WelcomeActivity.java
@@ -1,18 +1,26 @@
 package cn.edu.stu.max.mindmap;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
         setContentView(R.layout.activity_welcome);
-//        getSupportActionBar().hide();
-
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
